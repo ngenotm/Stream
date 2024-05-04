00b0ad2a1efa98db170a96abb98faa2acd589e0a
amirreza rostami <amirreza.rostami.0073@gmail.com>
2024-05-04 12:59:02 +0330

    -front single main content

--- frontend/src/components/SlidePagination.jsx
+++ frontend/src/components/SlidePagination.jsx
@@ -8,7 +8,7 @@ const SlidePagination = () => {
                 <button
                     className="w-10 h-10 rounded-lg bg-c-black-10 border border-c-black-12
                  flex justify-center items-center"
-                ><LeftArrowSvg /></button>
+                ><LeftArrowSvg className="stroke-white w-[18px] h-[18px]" /></button>
                 <div className="flex items-center gap-1">
                     <div className="h-[2px] min-w-3 w-5 bg-c-red-45"></div>
                     <div className="h-[2px] min-w-3 bg-c-black-20"></div>
@@ -18,7 +18,7 @@ const SlidePagination = () => {
                 <button
                     className="w-10 h-10 rounded-lg bg-c-black-10 border border-c-black-12
                  flex justify-center items-center"
-                ><RightArrowSvg /></button>
+                ><LeftArrowSvg className="stroke-white w-[18px] h-[18px] rotate-180" /></button>
             </div>
 
         </div>
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,6 +3,7 @@
 import { BellSvg, MenuSvg, SearchSvg } from "@/assets/Svgs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import NavbarNav from "./NavbarNav";
 
 
 const Navbar = () => {
@@ -12,7 +13,7 @@ const Navbar = () => {
     return (
         <header className={`py-5 ${pathname === "/" && "absolute"} top-0 right-0 z-30 w-full`}>
             <div
-                className="fixed w-full h-[5px] top-0 right-0
+                className="fixed w-full h-[5px] top-0 right-0 z-20
           3xl:bg-slate-500 2xl:bg-amber-400 xl:bg-stone-600 lg:bg-red-800 md:bg-orange-500 sm:bg-violet-500 bg-teal-400
         "></div>
             <div className="container flex items-center justify-between">
@@ -20,18 +21,8 @@ const Navbar = () => {
                     <img src="/images/logo-white.png" alt="StreamVibe" className="lg:w-[165px] w-[150px]" />
                 </Link>
 
-                <nav>
+                <NavbarNav pathname={pathname} />
 
-                    <div className="bg-c-black-06 border-2 border-c-black-12 rounded-lg p-2.5 md:block hidden">
-                        <ul className="flex items-center text-c-grey-75 xl:text-super-sm text-xs">
-                            <li className="lg:px-4 px-3.5 py-2 bg-c-black-10 rounded font-medium"><Link href="/">Home</Link></li>
-                            <li className="lg:px-4 px-3.5 py-2"><Link href="/">Movies & Shows</Link></li>
-                            <li className="lg:px-4 px-3.5 py-2"><Link href="/support">Support</Link></li>
-                            <li className="lg:px-4 px-3.5 py-2"><Link href="/subscriptions">Subscriptions</Link></li>
-                        </ul>
-                    </div>
-
-                </nav>
                 <div>
                     <button className="focus:outline-none focus:border-none mx-2.5 md:inline hidden"><SearchSvg /></button>
                     <button className="focus:outline-none focus:border-none mx-2.5 md:inline hidden"><BellSvg /></button>
