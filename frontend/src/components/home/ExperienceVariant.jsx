c7343e59a8b88d23267416226d82f62ce1c3bf33
amirreza rostami <amirreza.rostami.0073@gmail.com>
2024-05-05 11:06:38 +0330

    -front refactor experience section

---
+++ frontend/src/components/home/ExperienceVariant.jsx
@@ -0,0 +1,34 @@
+import { GameConsoleSvg, LaptopSvg, SmartPhoneSvg, SmartTvSvg, TabletSvg, VrHeadsetSvg } from "@/assets/Svgs";
+
+export const variant = [
+    {
+        icon: <SmartPhoneSvg />,
+        title: "Smartphones",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+    {
+        icon: <SmartTvSvg />,
+        title: "Smart TV",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+    {
+        icon: <TabletSvg />,
+        title: "Tablet",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+    {
+        icon: <LaptopSvg />,
+        title: "Laptops",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+    {
+        icon: <GameConsoleSvg />,
+        title: "Gaming Consoles",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+    {
+        icon: <VrHeadsetSvg />,
+        title: "VR Headsets",
+        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+    },
+];
--- frontend/src/components/home/HomeExperience.jsx
+++ frontend/src/components/home/HomeExperience.jsx
@@ -1,39 +1,7 @@
-import { GameConsoleSvg, LaptopSvg, SmartPhoneSvg, SmartTvSvg, TabletSvg, VrHeadsetSvg } from "@/assets/Svgs";
 import ExperienceItem from "./ExperienceItem";
 import ExperienceTitle from "./ExperienceTitle";
 
-const variant = [
-    {
-        icon: <SmartPhoneSvg />,
-        title: "Smartphones",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-    {
-        icon: <SmartTvSvg />,
-        title: "Smart TV",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-    {
-        icon: <TabletSvg />,
-        title: "Tablet",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-    {
-        icon: <LaptopSvg />,
-        title: "Laptops",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-    {
-        icon: <GameConsoleSvg />,
-        title: "Gaming Consoles",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-    {
-        icon: <VrHeadsetSvg />,
-        title: "VR Headsets",
-        subtitle: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
-    },
-];
+import { variant } from "./ExperienceVariant";
 
 
 const HomeExperience = () => {
